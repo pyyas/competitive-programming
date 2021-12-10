acdc179688669b7deb80c4bb0a4d3e88699d043e
pyyas <95765879+pyyas@users.noreply.github.com>
2021-12-10 22:57:33 +0300

    Create insertion.py

---
+++ week-1/insertion.py
@@ -0,0 +1,17 @@
+def insertionSort1(n, arr):
+   
+    num = arr[n-1]
+    i = n-2
+    while i>=0:
+        if arr[i]>num:
+            arr[i+1]=arr[i]
+            print(*arr)
+            i-=1
+        elif arr[i] <= num:
+            arr[i+1] = num
+            print(*arr)
+            i = -1
+            
+    if arr[0] > num:
+        arr[0] = num
+        print(*arr)
