1409603d55190ce39576ea726d3bdae3c565d65a
pyyas <95765879+pyyas@users.noreply.github.com>
2022-03-30 23:08:21 +0300

    Create first_bad.py

---
+++ Camp/Binary/first_bad.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo < hi:
+            mid = (lo+hi)//2
+            if isBadVersion(mid):
+                hi = mid
+            else:
+                lo = mid+1
+        return hi
